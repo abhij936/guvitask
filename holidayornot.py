d5eda27df56c083c5fe46e8a5e89b292a5076c4f
abhij936 <52398578+abhij936@users.noreply.github.com>
2019-07-04 09:36:13 +0530

    Create holidayornot.py

---
+++ holidayornot.py
@@ -0,0 +1,7 @@
+day=input("")
+if ((day=="Sunday") or (day=="Saturday")) :
+    print("yes")
+elif ((day=="Monday") or (day=="Tuesday") or (day=="Wednesday") or (day=="Thursday") or (day=="Friday")) :
+    print("no")
+else :
+    print("no")
