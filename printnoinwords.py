704a9ac34e8c15d3491269f61e3175c64c2941c8
abhij936 <52398578+abhij936@users.noreply.github.com>
2019-07-05 15:58:02 +0530

    Create printnoinwords.py

---
+++ printnoinwords.py
@@ -0,0 +1,15 @@
+thisdict =	{
+  1 : "One",
+  2 : "Two",
+  3 : "Three",
+  4 : "Four",
+  5 : "Five",
+  6 : "Six",
+  7 : "Seven",
+  8 : "Eight",
+  9 : "Nine",
+ 10 : "Ten"
+}
+a=int(input())
+x = thisdict.get(a)
+print(x)
