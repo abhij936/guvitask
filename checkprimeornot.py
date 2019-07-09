b2b51001eb5687713f785a79c63b992d17231155
abhij936 <52398578+abhij936@users.noreply.github.com>
2019-07-09 14:49:36 +0530

    Update checkprimeornot.py

--- checkprimeornot.py
+++ checkprimeornot.py
@@ -1,7 +1,7 @@
-num=int(input())
-if ((num>1) and (num<=1000)) :
-    for x in range(2,num) :
-        if (num%x==0) :
+num1=int(input())
+if ((num1>1) and (num1<=1000)) :
+    for x in range(2,num1) :
+        if (num1%x==0) :
             print("no")
             break
     else:
